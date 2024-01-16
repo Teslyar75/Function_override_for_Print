342a1c40518510f1fb3cd5029bbc047003372d5b
Teslyar <Teslyar@TESLYAR>
2024-01-17 00:29:32 +0200

    Добавьте файлы проекта.

---
+++ Function_override_for_Print.cpp
@@ -0,0 +1,47 @@
+﻿//2) Создать абстрактный базовый класс Employer(служащий) с чисто виртуальной функцией Print().
+// Создайте три производных класса : President, Manager, Worker.
+//Переопределите функцию Print() для вывода информации, соответствующей каждому типу служащего.
+
+
+#include <iostream>
+using namespace std;
+
+class Employer {
+public:
+    virtual void Print() const = 0;
+    virtual ~Employer() {}
+};
+
+class President : public Employer {
+public:
+    void Print() const override {
+        cout << "Президент: Высший исполнитель, великий мудрый кормчий, ответственный за общее направление развития.\n";
+    }
+};
+
+class Manager : public Employer {
+public:
+    void Print() const override {
+        cout << "Менеджер: Специалист, ответственный за планирование, организацию и управление работой группы людей." << endl;
+    }
+};
+
+class Worker : public Employer {
+public:
+    void Print() const override {
+        cout << "Работник: Сотрудник, занятый ручной или промышленной работой." << endl;
+    }
+};
+
+int main() {
+    setlocale(0, "");
+
+    President president;
+    Manager manager;
+    Worker worker;
+
+    cout << "Информация о сотрудниках компании:\n";
+    president.Print();
+    manager.Print();
+    worker.Print();
+}
